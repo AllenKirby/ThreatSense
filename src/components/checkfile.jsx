ccfff0249d97cd5eb2cedd15ebc567a91b4a5fdb
dinielle <cordondinielle00333@gmail.com>
2024-05-01 21:48:16 +0800

    api added

--- src/components/checkfile.jsx
+++ src/components/checkfile.jsx
@@ -1,20 +1,48 @@
 import { FiUpload } from 'react-icons/fi'
-
+import {useState} from 'react'
+import axios from 'axios';
 const Testnetwork = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
 
   const uploadFile = () =>{
     let input = document.createElement('input');
     input.type = 'file';
     input.accept = '.exe, .dll';
+    // input.addEventListener('change', handleFileChange)
     input.click();
   }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+        const response = await axios.post('https://maldetectionml01.pythonanywhere.com/extract', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+  };
 
   return (
     <section className="w-full h-4/5 flex items-center justify-center">
       <div className="w-auto h-auto text-center p-7">
         <p className="text-4xl font-semibold">Upload an .exe or . dll file to <br /> check for potential malware</p>
         <div className='w-full flex items-center justify-center'>
-          <button onClick={uploadFile} className="text-2xl px-7 py-2 rounded-2xl bg-cyan-950 mt-4 hover:bg-white hover:text-slate-900 hover:scale-125 transition-all duration-150 flex"><FiUpload color='white' className='mr-4'/> Upload File</button>
+          <form onSubmit={handleSubmit}>
+            <input type="file" accept=".exe,.dll" onChange={handleFileChange} />
+            <button type='submit' className="text-2xl px-7 py-2 rounded-2xl bg-cyan-950 mt-4 hover:bg-white hover:text-slate-900 hover:scale-125 transition-all duration-150 flex"><FiUpload color='white' className='mr-4'/> Upload File</button>
+          </form>
         </div>
       </div>
     </section>
